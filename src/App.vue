982bae07844ca67754de450ed11c51be66a19179
Qing Chen <j-qinche@microsoft.com>
2019-03-11 15:13:55 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="实例"></HelloWorld>
+    <P>{{plus(message)}}</P>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "app",
+  data() {
+    return {
+      message: "Hello"
+    };
+  },
+  components: {
+    HelloWorld
+  },
+  methods: {
+    plus(mes) {
+      return mes
+        .split("")
+        .reverse()
+        .join("");
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,40 @@
+<template>
+<div id="watch-example">
+  <p>
+    Ask a yes/no question:
+    <input v-model="message">
+  </p>
+  <p>{{ answer }}</p>
+</div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      message: "Word"
+    };
+  },
+};
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
